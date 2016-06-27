aead717099103e63474265a7b40be12ecda6257a
Zoe Lin <yicheng.zoe@gmail.com>
2016-06-27 10:36:14 +0800

    solved markers overload problem

--- Mr-Ride-iOS/MapViewController.swift
+++ Mr-Ride-iOS/MapViewController.swift
@@ -128,7 +128,7 @@ class MapViewController: UIViewController, UIPickerViewDataSource, UIPickerViewD
     }
     
     func getToiletAndStations() {
-        sendAGetRequestToServer()
+        sendAGetRequestToServer(.Toilets)
        
     }
     
@@ -155,12 +155,14 @@ class MapViewController: UIViewController, UIPickerViewDataSource, UIPickerViewD
         switch row {
         case 0:
             dataType = PickerViewCases.UbikeStations
+            sendAGetRequestToServer(.UbikeStations)
            
         default:
             dataType = PickerViewCases.Toilets
+            setMarkers(dataType)
         }
   
-        setMarkers(dataType)
+        
     }
     
     
@@ -264,26 +266,26 @@ extension MapViewController: CLLocationManagerDelegate, GMSMapViewDelegate {
     func setMarkers(pickerViewData: PickerViewCases){
         setMapDelegation()
         mapView.clear()
-        let imageViewToilet = UIImageView(image: UIImage(named: "icon-toilet"))
-        let imageViewBike = UIImageView(image: UIImage(named: "icon-bike"))
-        let markerBase = UIView()
-        markerBase.frame.size = CGSize(width: 40, height: 40)
-        markerBase.layer.cornerRadius = 20
-        markerBase.backgroundColor = UIColor.whiteColor()
-        
+
         switch pickerViewData {
         case .Toilets:
+            mapView.clear()
             if downtownToilets.count > 0 {
-                
                 for toilet in downtownToilets {
                     if locationManager.location?.distanceFromLocation(CLLocation(latitude: toilet.latitude, longitude: toilet.longitude)) < 1000 {
+                        let imageViewToilet = UIImageView(image: UIImage(named: "icon-toilet"))
                         let  position = CLLocationCoordinate2DMake(toilet.latitude, toilet.longitude)
                         let marker = GMSMarker(position: position)
-                
+                        let markerBase = UIView()
+                        markerBase.frame.size = CGSize(width: 40, height: 40)
+                        markerBase.layer.cornerRadius = 20
+                        markerBase.backgroundColor = UIColor.whiteColor()
+                        imageViewToilet.frame.origin.x = markerBase.frame.origin.x + 10
                         imageViewToilet.frame.origin.y = markerBase.frame.origin.y + 10
-                        markerBase.addSubview(imageViewToilet)
+                       
                         marker.iconView = markerBase
                         marker.title = toilet.name
+                        markerBase.addSubview(imageViewToilet)
                         
                         var myData = Dictionary<String, AnyObject>()
                         myData["name"] = toilet.name
@@ -292,32 +294,37 @@ extension MapViewController: CLLocationManagerDelegate, GMSMapViewDelegate {
                         marker.userData = myData
                         marker.map = mapView
                     }
-                    //marker.icon = UIImage(named: "icon-toilet")
-                    
                 }
             }
         case .UbikeStations:
+            mapView.clear()
+            //marker.map = nil
+//            
+//            let url = NSBundle.mainBundle().URLForResource("YouBikeTP", withExtension: "gz") // call Json file
+//            
+//            let data = NSData(contentsOfURL: url!)
+//            if let dataObject =  try? NSJSONSerialization.JSONObjectWithData(data!, options: .AllowFragments) {
+//                getDataFromJson(.UbikeStations, object: dataObject)
+//                
+//            }
             
-            let url = NSBundle.mainBundle().URLForResource("YouBikeTP", withExtension: "gz") // call Json file
-            
-            let data = NSData(contentsOfURL: url!)
-            if let dataObject =  try? NSJSONSerialization.JSONObjectWithData(data!, options: .AllowFragments) {
-                getDataFromJson(.UbikeStations, object: dataObject)
-                
-            }
-            
-            fetchCoreData(.UbikeStations)
-
+            //fetchCoreData(.UbikeStations)
             if stations.count > 0 {
                 for station in stations {
                     if locationManager.location?.distanceFromLocation(CLLocation(latitude: station.latitude, longitude: station.longitude)) < 1000 {
+                        let imageViewBike = UIImageView(image: UIImage(named: "icon-bike"))
                         let  position = CLLocationCoordinate2DMake(station.latitude, station.longitude)
                         let marker = GMSMarker(position: position)
- 
-                        markerBase.addSubview(imageViewBike)
+                        let markerBase = UIView()
+                        markerBase.frame.size = CGSize(width: 40, height: 40)
+                        markerBase.layer.cornerRadius = 20
+                        markerBase.backgroundColor = UIColor.whiteColor()
+                        imageViewBike.frame.origin.x = markerBase.frame.origin.x + 5
+                        imageViewBike.frame.origin.y = markerBase.frame.origin.y + 5
+                        
                         marker.iconView = markerBase
                         marker.title = station.name
-                        
+                        markerBase.addSubview(imageViewBike)
                         var myData = Dictionary<String, AnyObject>()
                         myData["name"] = station.name
                         myData["address"] = station.address
@@ -326,7 +333,6 @@ extension MapViewController: CLLocationManagerDelegate, GMSMapViewDelegate {
                         marker.map = mapView
 
                     }
-                    
                 }
             }
             
@@ -336,14 +342,19 @@ extension MapViewController: CLLocationManagerDelegate, GMSMapViewDelegate {
 
 // Model
 extension MapViewController: NSFetchedResultsControllerDelegate {
-    func sendAGetRequestToServer() {
+    func sendAGetRequestToServer(pickerViewData: PickerViewCases) {
         dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)) {
-            // request Json file from internet
-            //let code = self.SetJWTCode()
-            let url = "http://data.taipei/opendata/datalist/apiAccess?scope=resourceAquire&rid=008ed7cf-2340-4bc4-89b0-e258a5573be2"
-//            func request(URLRequest: URLRequestConvertible) -> Request {
-//                return Manager.sharedInstance.request(URLRequest.URLRequest)
-//            }
+            var url = ""
+            switch pickerViewData {
+            case .Toilets:
+                url = "http://data.taipei/opendata/datalist/apiAccess?scope=resourceAquire&rid=008ed7cf-2340-4bc4-89b0-e258a5573be2"
+                //            func request(URLRequest: URLRequestConvertible) -> Request {
+                //                return Manager.sharedInstance.request(URLRequest.URLRequest)
+            //            }
+            case.UbikeStations:
+                url = "http://data.taipei/youbike"
+            }
+            
             Alamofire.request(.GET, url)
                 //.validate()
                 .responseJSON {response in
@@ -353,11 +364,11 @@ extension MapViewController: NSFetchedResultsControllerDelegate {
                         if let dictionary = response.result.value{
                             print("get JSON data online sucessfull")
                            // print(dictionary)
-                            self.getDataFromJson(PickerViewCases.Toilets, object: dictionary)
-                            self.fetchCoreData(.Toilets)
+                            self.getDataFromJson(pickerViewData, object: dictionary)
+                            self.fetchCoreData(pickerViewData)
                             dispatch_async(dispatch_get_main_queue(), {
                                 
-                                self.setMarkers(.Toilets)
+                                self.setMarkers(pickerViewData)
                                 
                             })
                             //self.self.getFollowingPageData()
@@ -380,23 +391,44 @@ extension MapViewController: NSFetchedResultsControllerDelegate {
     
     
     // Fetch URL Data and Handle CoreData
-    func cleanUpCoreData() {
-        if let managedObjectContext = (UIApplication.sharedApplication().delegate as? AppDelegate)?.managedObjectContext {
-            let request = NSFetchRequest(entityName: "DowntownToilet")
-            do {
-                let results = try managedObjectContext.executeFetchRequest(request) as! [DowntownToilet]
-                for result in results {
-                    managedObjectContext.deleteObject(result)
-                }
+    func cleanUpCoreData(Cases: PickerViewCases) {
+        switch Cases {
+        case .UbikeStations:
+            if let managedObjectContext = (UIApplication.sharedApplication().delegate as? AppDelegate)?.managedObjectContext {
+                let request = NSFetchRequest(entityName: "Stations")
                 do {
-                    try managedObjectContext.save()
+                    let results = try managedObjectContext.executeFetchRequest(request) as! [Stations]
+                    for result in results {
+                        managedObjectContext.deleteObject(result)
+                    }
+                    do {
+                        try managedObjectContext.save()
+                    }catch{
+                        fatalError("Failure to save context: \(error)")
+                    }
                 }catch{
-                    fatalError("Failure to save context: \(error)")
+                    fatalError("Failed to fetch data: \(error)")
                 }
-            }catch{
-                fatalError("Failed to fetch data: \(error)")
+            }
+        default:
+            if let managedObjectContext = (UIApplication.sharedApplication().delegate as? AppDelegate)?.managedObjectContext {
+                let request = NSFetchRequest(entityName: "DowntownToilet")
+                do {
+                    let results = try managedObjectContext.executeFetchRequest(request) as! [DowntownToilet]
+                    for result in results {
+                        managedObjectContext.deleteObject(result)
+                    }
+                    do {
+                        try managedObjectContext.save()
+                    }catch{
+                        fatalError("Failure to save context: \(error)")
+                    }
+                }catch{
+                    fatalError("Failed to fetch data: \(error)")
+                }
             }
         }
+
     }
     
 
@@ -441,12 +473,13 @@ extension MapViewController: NSFetchedResultsControllerDelegate {
     func getInfo(Cases: PickerViewCases, results: [AnyObject]) {
         switch Cases {
         case .UbikeStations:
-            //print (results)
+            cleanUpCoreData(.UbikeStations)
+            print (results.count)
+            
             for eachStationData in results {
-                //print(eachStationData)
                 if let managedObjectContext = (UIApplication.sharedApplication().delegate as? AppDelegate)?.managedObjectContext {
                     station = NSEntityDescription.insertNewObjectForEntityForName("Stations", inManagedObjectContext: managedObjectContext) as! Stations
-                    
+
                     station.name = eachStationData["snaen"] as! String
                     station.address = eachStationData["aren"] as! String
                     station.dist = eachStationData["sareaen"] as! String
@@ -466,6 +499,7 @@ extension MapViewController: NSFetchedResultsControllerDelegate {
                     do{
                         try managedObjectContext.save()
                         
+                        
                     } catch {
                         print(error)
                         return
@@ -476,11 +510,9 @@ extension MapViewController: NSFetchedResultsControllerDelegate {
             }
             
         default:
-            cleanUpCoreData()
+            cleanUpCoreData(.Toilets)
+
             for eachToiletData in results {
-                // save eachData in CoreData
-                //print(eachToiletData)
-                
                 if let managedObjectContext = (UIApplication.sharedApplication().delegate as? AppDelegate)?.managedObjectContext {
                     downtownToilet = NSEntityDescription.insertNewObjectForEntityForName("DowntownToilet", inManagedObjectContext: managedObjectContext) as! DowntownToilet
                     downtownToilet.name = eachToiletData["\u{55ae}\u{4f4d}\u{540d}\u{7a31}"] as! String
@@ -531,6 +563,7 @@ extension MapViewController: NSFetchedResultsControllerDelegate {
                     try fetchResultController.performFetch()
                     
                     stations = fetchResultController.fetchedObjects as! [Stations]
+                    print (stations.count)
                     
                     
                 } catch {
